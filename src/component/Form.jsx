06d5404070fa53fbf594d98ee7a0bcfd513df6a1
Hala <hm1316572@gmail.com>
2026-02-03 15:24:25 +0200

    responsive

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -24,7 +24,7 @@ return (
             <h6 className="text-[12px] pt-14 tracking-[6px] uppercase">35,000+ already joined</h6>
             <h1 className="py-4 text-3xl">Stay up-to-date with what <br /> we’re doing</h1>
 
-            <form className="mt-6 flex flex-col md:flex-row gap-6 relative" onSubmit={handleSupmite}>
+            <form className="mt-6 flex flex-col px-6 md:px-0 md:flex-row gap-6 relative" onSubmit={handleSupmite}>
                 <input value={email} onChange={(e)=> setEmail(e.target.value)} className="bg-white w-full py-3 md:w-[250px] rounded-sm focus:outline-none text-gray-500 md:py-2 px-4 text-sm" type="text" placeholder="Enter Your email address.." />
                 {error && <p className="absolute left-0 top-[45px] w-full bg-red-500 text-white text-xs py-1 px-2 rounded-sm">{error}</p>}
                 <button className="bg-red-400 mb-9  text-white py-3 md:py-2 px-4 rounded-sm cursor-pointer">Contact Us</button>
